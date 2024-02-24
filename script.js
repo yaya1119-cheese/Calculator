d8db6e10b0c3201091fdfc14f7b6b88e54102ad0
Yaya Gusfa <yayag1900@gmail.com>
2024-02-24 13:31:50 -0500

    simple message

--- script.js
+++ script.js
@@ -1,23 +1,53 @@
-const operator = input('Enter +, -, *, or /: ');
+// const operator = prompt('Enter +, -, *, or /: ');
 
-const number1 = float(input('Enter first number: '));
-const number2 = float(input('Enter second number: '));
+// const number1 = Number(prompt('Enter first number: '));
+// const number2 = Number(prompt('Enter second number: '));
 
-let result;
+// let result;
 
 
-if (operator == '+') {
-    result = number1 + number2;
+// if (operator == '+') {
+//     result = number1 + number2;
+// }
+// else if (operator == '-') {
+//     result = number1 - number2;
+// }
+// else if (operator == '*') {
+//     result = number1 * number2;
+// }
+// else {
+//     result = number1 / number2;
+// }
+
+
+// alert(`${result}`);
+
+
+
+// function append(){
+
+
+// }
+
+let doc = document
+
+console.log(doc)
+
+let calc = document.getElementById('calculator')
+
+console.log(calc)
+
+function appendToDisplay(value){
+    document.getElementById('display').value += value;
 }
-else if (operator == '-') {
-    result = number1 - number2;
-}
-else if (operator == '*') {
-    result = number1 * number2;
-}
-else {
-    result = number1 / number2;
-}
 
 
-console.log(`${number1} ${operator} ${number2} = ${result}`);
+function clearDisplay(){
+    document.getElementById('display').value = ''
+
+}
+
+function calculateResult(){
+    var result = eval(document.getElementById('display').value)
+    document.getElementById('display').value = result
+}
